96199c66f9943edd5ab37b70c51a80bd652801d9
Vlad Tiazhov <tva1992@gmail.com>
2024-02-03 18:59:19 +0200

    RUN.py

--- RUN.py
+++ RUN.py
@@ -40,29 +40,30 @@ def get_all_spiders():
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Run scrapy spiders for different states and proceedings.")
+    parser = argparse.ArgumentParser(description="Run Scrapy spiders for different states.")
     parser.add_argument('name', nargs='?', choices=[*configurations.keys(), 'ALL'], help='The name of the script to run')
     args = parser.parse_args()
 
     # Dynamically generate choices and matching cases
+    selected_key = None
     if args.name:
-        if args.name == 'ALL':
-            run_spiders(get_all_spiders())
-        elif args.name in configurations:
-            run_spiders(configurations[args.name].spiders)
+        selected_key = args.name
     else:
         choices = [f"Press {index + 1} for {name}" for index, name in enumerate(['ALL', *configurations.keys()])]
         choices_text = "\n".join(choices)
         choice_input = int(input(f"{choices_text}\nYour choice:\n"))
-
-        print(configurations)
         if choice_input == 1:
-            run_spiders(get_all_spiders())
+            selected_key = get_all_spiders()
         elif 2 <= choice_input < len(configurations) + 2:
-            selected = list(configurations.keys())[choice_input - 2]
-            run_spiders(configurations[selected].spiders)
-        else:
-            print("Invalid choice. Exiting.")
+            selected_key = list(configurations.keys())[choice_input - 2]
+
+    # run
+    if selected_key == 'ALL':
+        run_spiders(get_all_spiders())
+    elif selected_key in configurations:
+        run_spiders(configurations[args.name].spiders)
+    else:
+        print("Invalid choice. Exiting.")
 
 
 if __name__ == '__main__':
