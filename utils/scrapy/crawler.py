4153a1609af66c5af021ab2ed1660e66d4d29d7c
Vlad Tiazhov <tva1992@gmail.com>
2024-03-14 19:41:02 +0200

    Add new helpers

--- utils/scrapy/crawler.py
+++ utils/scrapy/crawler.py
@@ -1,17 +1,35 @@
 from scrapy.crawler import Spider, CrawlerProcess, CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor, defer
+import logging
+import crochet
 
-from typing import Type
+
+def crawl_with_crochet(spider: type[Spider] | str, *args, **kwargs):
+    try:
+        crochet.setup()   # avoids ReactorNotRestartable error
+
+        @crochet.wait_for(timeout=60 * 60 * 5)  # Wait for spider completion for 5 hours before throwing TimeoutError
+        def run_in_reactor():
+            runner = CrawlerRunner(get_project_settings())
+            return runner.crawl(spider, *args, **kwargs)  # return the Deferred that fires when the crawl is done
+
+        # block until the Deferred returned by runner.crawl() fires
+        run_in_reactor()
+        logging.info(f'Spider {spider} finished')
+
+    except Exception as e:
+        logging.exception(e)
+        raise
 
 
-def crawl(spider: Type[Spider], **kwargs):
+def crawl(spider: type[Spider], **kwargs):
     process = CrawlerProcess(get_project_settings(), install_root_handler=False)
     process.crawl(spider, **kwargs)
     process.start()
 
 
-def crawl_parallel(*spiders: Type[Spider]):
+def crawl_parallel(*spiders: type[Spider]):
     """https://docs.scrapy.org/en/latest/topics/practices.html#running-multiple-spiders-in-the-same-process"""
     process = CrawlerProcess(get_project_settings(), install_root_handler=False)
     for spider in spiders:
@@ -19,7 +37,7 @@ def crawl_parallel(*spiders: Type[Spider]):
     process.start()
 
 
-def crawl_sequential(*spiders: Type[Spider]):
+def crawl_sequential(*spiders: type[Spider]):
     """https://docs.scrapy.org/en/latest/topics/practices.html#running-multiple-spiders-in-the-same-process"""
     runner: CrawlerRunner = CrawlerRunner(get_project_settings())
 
--- utils/types/date.py
+++ utils/types/date.py
@@ -1,6 +1,46 @@
 from datetime import datetime
 
 
+TOP_TIMEZONES = [
+    "America/Chicago",
+    "America/Los_Angeles",
+    "America/Mexico_City",
+    "America/New_York",
+    "America/Sao_Paulo",
+    "America/Toronto",
+    "Europe/Amsterdam",
+    "Europe/Berlin",
+    "Europe/Istanbul",
+    "Europe/Kyiv",
+    "Europe/London",
+    "Europe/Madrid",
+    "Europe/Oslo",
+    "Europe/Paris",
+    "Europe/Rome",
+    "Europe/Stockholm",
+    "Europe/Warsaw",
+    "Europe/Zurich",
+    "Asia/Bangkok",
+    "Asia/Dhaka",
+    "Asia/Dubai",
+    "Asia/Ho_Chi_Minh",
+    "Asia/Hong_Kong",
+    "Asia/Jakarta",
+    "Asia/Karachi",
+    "Asia/Kolkata",
+    "Asia/Kuala_Lumpur",
+    "Asia/Riyadh",
+    "Asia/Seoul",
+    "Asia/Shanghai",
+    "Asia/Singapore",
+    "Asia/Taipei",
+    "Asia/Tel_Aviv",
+    "Asia/Tokyo",
+    "Australia/Sydney",
+    "Africa/Johannesburg",
+]
+
+
 def format_date_string_to_iso(date_string: str, source_format: str) -> str:
     """Parse and convert date string to ISO pretty format (2023-12-17)"""
     try:
