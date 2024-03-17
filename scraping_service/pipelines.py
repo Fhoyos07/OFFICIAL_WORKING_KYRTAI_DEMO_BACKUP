d43e9d14e86100066610ca5a8240820bdd609340
Vlad Tiazhov <tva1992@gmail.com>
2024-03-17 23:03:59 +0200

    Set common case fields on pipeline level

--- scraping_service/pipelines.py
+++ scraping_service/pipelines.py
@@ -38,6 +38,8 @@ class CaseSearchDbPipeline(BasePipeline):
     def process_item(self, item: DbItem, spider: BaseCaseSearchSpider):
         # convert item to Case model and append to list for bulk processing
         case: Case = item.record
+        case.found_date = timezone.now()
+
         self.cases_to_create.append(case)
 
         if len(self.cases_to_create) >= self.chunk_size:
@@ -89,6 +91,8 @@ class CaseDetailDbPipeline(BasePipeline):
         self.create_documents()
 
     def update_case(self, case: Case):
+        case.is_scraped = True
+        case.scraped_date = timezone.now()
         case.save()
 
         case_detail = getattr(case, self.case_detail_relation)
@@ -137,7 +141,7 @@ class DocumentS3UploadPipeline(BasePipeline):
         relative_path = f"{spider.state_code}/{document.case.docket_id}/{document.document_id}.pdf"
         try:
             self.article_bucket.put_object(Key=relative_path, Body=item.body)
-            self.logger.info(f"Uploaded to S3 {self.s3_bucket_name} at {relative_path}")
+            self.logger.debug(f"Uploaded to S3 {self.s3_bucket_name} at {relative_path}")
             item.relative_path = relative_path
         except Exception as e:
             self.logger.error(f"Failed to save {relative_path} to S3: {e}")
@@ -169,113 +173,3 @@ class DocumentDbPipeline(BasePipeline):
         if not self.documents_to_update: return
         Document.objects.bulk_update(self.documents_to_update, fields=['is_downloaded', 'relative_path'])
         self.logger.info(f'Updated {len(self.documents_to_update)} Documents')
-
-
-# class LoggingPipeline:
-#     def process_item(self, item, spider):
-#         item_dict = ItemAdapter(item).asdict()
-#         spider.logger.info(json.dumps(item_dict, indent=2))
-#         return item
-#
-#
-# class DocumentSavePipeline(BasePipeline):
-#     def __init__(self, spider: Spider):
-#         super().__init__(spider)
-#         self.pdf_dir = Path(self.settings['FILES_DIR']) / spider.state_code / 'pdfs'
-#
-#     def process_item(self, item: dict, spider: Spider):
-#         # generate pdf path
-#         pdf_path = self.pdf_dir / item['relative_file_path']
-#         pdf_path.parent.mkdir(parents=True, exist_ok=True)
-#
-#         # save to file
-#         with open(pdf_path, 'wb') as file:
-#             file.write(item['body'])
-#         self.logger.debug(f"Wrote to {pdf_path}")
-#
-#
-# class CsvPipeline(BasePipeline):
-#     """
-#     Pipeline for writing to CSV file during spider crawl.
-#     Support writing multiple item types simultaneously.
-#     """
-#     def __init__(self, spider: Spider):
-#         super().__init__(spider)
-#         if not self.settings.get('FILES_DIR'):
-#             raise ValueError('FILES_DIR not specified in settings')
-#
-#         self.csv_dir: Path = self.settings['FILES_DIR'] / spider.state_code
-#         self.csv_dir.mkdir(parents=True, exist_ok=True)
-#         self.logger.info(f"Using {self.csv_dir} as dir for CSV export")
-#
-#         self.csv_writer_by_type: dict[str, CsvWriterWrapper] = {}
-#
-#     def process_item(self, item: CaseItem, spider):
-#         self.logger.info(f"item: {item}")
-#         # use item._type to group items, or just spider.name
-#         from dataclasses import asdict
-#         self.logger.info(f"item: {item}")
-#         if isinstance(item, CaseItem):
-#             item_type = 'Cases'
-#         elif isinstance(item, DocumentItem):
-#             item_type = 'Documents'
-#         else:
-#             raise TypeError(f'Unrecognized item type: {item}')
-#
-#         item_dict = asdict(item)
-#         self.logger.info(f"item_dict: {item_dict}")
-#         csv_row = item_dict | item_dict.pop('state_specific_info')
-#
-#         # get CSV file writer for item type, or start a new one
-#         csv_writer: CsvWriterWrapper = self.csv_writer_by_type.get(item_type)
-#         if csv_writer is None:
-#             csv_writer = self._open_csv_writer(csv_name=item_type, fieldnames=list(csv_row.keys()), write_header=True)
-#             self.csv_writer_by_type[item_type] = csv_writer
-#
-#         # write item to CSV
-#         csv_writer.writerow(csv_row)
-#         return item
-#
-#     def close_spider(self, spider):
-#         for csv_writer in self.csv_writer_by_type.values():
-#             csv_writer.close()
-#
-#     def _open_csv_writer(self, csv_name: str, fieldnames: list[str], write_header: bool = False) -> 'CsvWriterWrapper':
-#         csv_path = self.csv_dir / f'{csv_name.lower()}.csv'
-#         self.logger.info(f"Opening CSV writer at {csv_path} with fieldnames:\n {','.join(fieldnames)}")
-#         csv_writer = CsvWriterWrapper(csv_path=csv_path, fieldnames=fieldnames, override=False)
-#         if write_header:
-#             csv_writer.writeheader()
-#         return csv_writer
-#
-#
-# class CsvOnClosePipeline(CsvPipeline):
-#     """
-#     Pipeline for writing to CSV file, collecting all items and writing them at once on spider finish.
-#     Support writing only 1 writing type.
-#     """
-#     def __init__(self, spider: Spider):
-#         super().__init__(spider)
-#         self.items = []
-#
-#     def process_item(self, item, spider):
-#         self.items.append(item)
-#         return item
-#
-#     def close_spider(self, spider):
-#         fields_to_export = self.collect_fields(self.items)
-#         csv_writer = self._open_csv_writer(csv_name=spider.name,
-#                                            fieldnames=fields_to_export,
-#                                            write_header=True)
-#         csv_writer.writerows(self.items)
-#         csv_writer.close()
-#
-#     @staticmethod
-#     def collect_fields(items: list[dict]) -> list[str]:
-#         """Get list of all fields from all items (used as csv header)"""
-#         fields = []
-#         for item in items:
-#             for field in item:
-#                 if field not in fields:
-#                     fields.append(field)
-#         return fields
