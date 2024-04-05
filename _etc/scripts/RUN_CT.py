3b4ca4c263c20e2e3b1bdf1bc0d0d993af9a7400
Vlad Tiazhov <tva1992@gmail.com>
2024-04-05 10:34:03 -0300

    Test scripts

--- _etc/scripts/RUN_CT.py
+++ _etc/scripts/RUN_CT.py
@@ -1,9 +1,3 @@
-from pathlib import Path
-import sys
-PROJECT_DIR = Path(__file__).parent.parent.parent
-sys.path.append(str(PROJECT_DIR))
-
-
 from utils.scrapy.crawler import crawl_sequential, crawl, crawl_with_crochet
 from utils.django import django_setup_decorator
 
@@ -16,7 +10,7 @@ def run():
     # Case.objects.filter(state__code='CT').delete()
 
     # crawl_with_crochet(CtCaseSearchSpider, company_ids=[907])
-    crawl_with_crochet(CtCaseDetailSpider, company_ids=[907], scrape_all=True)
+    crawl_with_crochet(CtCaseDetailSpider, company_ids=[907], mode='new')
     # crawl_with_crochet(CtDocumentSpider)
 
 
--- _etc/scripts/RUN_NY.py
+++ _etc/scripts/RUN_NY.py
@@ -1,15 +1,13 @@
-from utils.scrapy.crawler import crawl_sequential
+from utils.scrapy.crawler import crawl_sequential, crawl, crawl_with_crochet
 from utils.django import django_setup_decorator
 
 
 @django_setup_decorator(environment='dev')
 def run():
     from scraping_service.spiders.spider_ny import NyCaseSearchSpider, NyCaseDetailSpider, NyDocumentSpider
-    crawl_sequential(
-        NyCaseSearchSpider,
-        NyCaseDetailSpider,
-        NyDocumentSpider
-    )
+    # crawl_with_crochet(NyCaseSearchSpider, company_ids=[907])
+    crawl_with_crochet(NyCaseDetailSpider, mode='not_assigned')
+    crawl_with_crochet(NyDocumentSpider)
 
 
 if __name__ == '__main__':
